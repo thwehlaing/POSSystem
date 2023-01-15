e401b3c5b7c693733a6c2acd184d08226fd45558
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2023-01-15 19:23:15 +0630

    by omk

--- POSMenu/frmUserLogin.Designer.cs
+++ POSMenu/frmUserLogin.Designer.cs
@@ -136,6 +136,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmUserLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "User Login";
