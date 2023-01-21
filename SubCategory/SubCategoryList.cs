12e4eb56ddda5b7b61a37dc73d250d8b2bbc84d7
Ohnmar Kyi <capital.ohnmarkyi@gmail.com>
2023-01-21 21:11:33 +0630

    by omk

---
+++ SubCategory/SubCategoryList.cs
@@ -0,0 +1,21 @@
+﻿using POSBase;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubCategory
+{
+    public partial class SubCategoryList : BaseForm
+    {
+        public SubCategoryList()
+        {
+            InitializeComponent();
+        }
+    }
+}
