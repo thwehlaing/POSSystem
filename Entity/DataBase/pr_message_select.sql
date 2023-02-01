350c5054172a48f7b53ebc78ed96694c037eea9d
thwehlaing <thwehlaing.1993@gmail.com>
2023-02-01 21:48:36 +0630

    by omk

---
+++ Entity/DataBase/pr_message_select.sql
@@ -0,0 +1,25 @@
+﻿ BEGIN TRY 
+ Drop Procedure [dbo].[pr_message_select]
+END try
+BEGIN CATCH END CATCH 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[pr_message_select]
+	-- Add the parameters for the stored procedure here
+	@MessageID varchar(4)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	Select * from Message where MessageID=@MessageID
+END
