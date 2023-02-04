ac0f2c803c24c4bea5a6d49ab03d98ed834d945e
thwehlaing <thwehlaing.1993@gmail.com>
2023-02-04 14:15:39 +0630

    by omk

---
+++ Entity/DataBase/pr_subcategory_insert.sql
@@ -0,0 +1,29 @@
+﻿ BEGIN TRY 
+ Drop Procedure [dbo].[pr_subcategory_insert]
+END try
+BEGIN CATCH END CATCH 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[pr_subcategory_insert] 
+	-- Add the parameters for the stored procedure here
+	@categorycd varchar(10),
+	@subname	nvarchar(50),	
+	@status	varchar(10),
+	@createddate	datetime,
+	@createduser	varchar(50)	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+   insert into SubCategory(SubCode,CategoryCD,SubName,Status,CreatedDate,CreatedUser) values('S00000001',@categorycd,@subname,@status,@createddate,@createduser)
+END
+GO
