410cd1afbbd8ad56a833ec099f70cac842cb881b
thwehlaing <thwehlaing.1993@gmail.com>
2023-02-11 22:18:30 +0630

    by omk

--- SubCategory/SubCategoryList.Designer.cs
+++ SubCategory/SubCategoryList.Designer.cs
@@ -34,6 +34,10 @@
             this.pButton1 = new POS_Control.PButton();
             this.dgvSubcategory = new POS_Control.PGridView();
             this.pTextBox1 = new POS_Control.PTextBox();
+            this.SubCatgoryCD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CategoryName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubCategory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Action = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSubcategory)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,9 +47,9 @@
             this.pLabel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pLabel1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.pLabel1.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.pLabel1.Location = new System.Drawing.Point(47, 84);
+            this.pLabel1.Location = new System.Drawing.Point(47, 80);
             this.pLabel1.Name = "pLabel1";
-            this.pLabel1.Size = new System.Drawing.Size(100, 30);
+            this.pLabel1.Size = new System.Drawing.Size(100, 29);
             this.pLabel1.TabIndex = 40;
             this.pLabel1.Text = "အမျိုးအစား";
             this.pLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -56,7 +60,7 @@
             this.pCombo1.FormattingEnabled = true;
             this.pCombo1.IsDatatableOccurs = null;
             this.pCombo1.IsErrorOccurs = false;
-            this.pCombo1.Location = new System.Drawing.Point(147, 84);
+            this.pCombo1.Location = new System.Drawing.Point(147, 80);
             this.pCombo1.MinimumSize = new System.Drawing.Size(100, 0);
             this.pCombo1.Name = "pCombo1";
             this.pCombo1.NextControl = null;
@@ -70,9 +74,9 @@
             this.pLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.pLabel2.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.pLabel2.Location = new System.Drawing.Point(473, 83);
+            this.pLabel2.Location = new System.Drawing.Point(473, 80);
             this.pLabel2.Name = "pLabel2";
-            this.pLabel2.Size = new System.Drawing.Size(100, 30);
+            this.pLabel2.Size = new System.Drawing.Size(100, 29);
             this.pLabel2.TabIndex = 42;
             this.pLabel2.Text = "အမျိုးအစား(ခွဲ)";
             this.pLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -83,20 +87,27 @@
             this.pButton1.ButtonType = Entity.ButtonType.BType.Normal;
             this.pButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.pButton1.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.pButton1.Location = new System.Drawing.Point(899, 81);
+            this.pButton1.Location = new System.Drawing.Point(899, 78);
             this.pButton1.Name = "pButton1";
-            this.pButton1.Size = new System.Drawing.Size(115, 30);
+            this.pButton1.Size = new System.Drawing.Size(115, 29);
             this.pButton1.TabIndex = 44;
             this.pButton1.Text = "ပြပါ";
             this.pButton1.UseVisualStyleBackColor = false;
             // 
             // dgvSubcategory
             // 
+            this.dgvSubcategory.AllowUserToAddRows = false;
             this.dgvSubcategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvSubcategory.Location = new System.Drawing.Point(44, 135);
+            this.dgvSubcategory.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SubCatgoryCD,
+            this.CategoryName,
+            this.SubCategory,
+            this.Action});
+            this.dgvSubcategory.Location = new System.Drawing.Point(44, 129);
             this.dgvSubcategory.Name = "dgvSubcategory";
+            this.dgvSubcategory.RowHeadersVisible = false;
             this.dgvSubcategory.RowTemplate.Height = 25;
-            this.dgvSubcategory.Size = new System.Drawing.Size(1244, 828);
+            this.dgvSubcategory.Size = new System.Drawing.Size(1472, 794);
             this.dgvSubcategory.TabIndex = 45;
             // 
             // pTextBox1
@@ -106,7 +117,7 @@
             this.pTextBox1.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.pTextBox1.IsDatatableOccurs = null;
             this.pTextBox1.IsErrorOccurs = false;
-            this.pTextBox1.Location = new System.Drawing.Point(573, 83);
+            this.pTextBox1.Location = new System.Drawing.Point(573, 80);
             this.pTextBox1.MinimumSize = new System.Drawing.Size(100, 20);
             this.pTextBox1.Name = "pTextBox1";
             this.pTextBox1.NextControl = null;
@@ -116,17 +127,42 @@
             this.pTextBox1.TabIndex = 46;
             this.pTextBox1.TextBoxType = POS_Control.PTextBox.PTextBoxType.Normal;
             // 
+            // SubCatgoryCD
+            // 
+            this.SubCatgoryCD.HeaderText = "အမျိုးအစားခွဲကုဒ်နံပါတ်";
+            this.SubCatgoryCD.Name = "SubCatgoryCD";
+            this.SubCatgoryCD.Width = 300;
+            // 
+            // CategoryName
+            // 
+            this.CategoryName.HeaderText = "အမျိုးအစား";
+            this.CategoryName.Name = "CategoryName";
+            this.CategoryName.Width = 500;
+            // 
+            // SubCategory
+            // 
+            this.SubCategory.HeaderText = "အမျိုးအစားခွဲ";
+            this.SubCategory.Name = "SubCategory";
+            this.SubCategory.Width = 500;
+            // 
+            // Action
+            // 
+            this.Action.HeaderText = "လုပ်ဆောင်ချက်";
+            this.Action.Name = "Action";
+            this.Action.Width = 200;
+            // 
             // SubCategoryList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1061);
+            this.ClientSize = new System.Drawing.Size(1924, 1017);
             this.Controls.Add(this.pTextBox1);
             this.Controls.Add(this.dgvSubcategory);
             this.Controls.Add(this.pButton1);
             this.Controls.Add(this.pLabel2);
             this.Controls.Add(this.pCombo1);
             this.Controls.Add(this.pLabel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
             this.Name = "SubCategoryList";
             this.Text = "SubCategoryList";
@@ -150,5 +186,9 @@
         private POS_Control.PButton pButton1;
         private POS_Control.PGridView dgvSubcategory;
         private POS_Control.PTextBox pTextBox1;
+        private DataGridViewTextBoxColumn SubCatgoryCD;
+        private DataGridViewTextBoxColumn CategoryName;
+        private DataGridViewTextBoxColumn SubCategory;
+        private DataGridViewTextBoxColumn Action;
     }
 }
