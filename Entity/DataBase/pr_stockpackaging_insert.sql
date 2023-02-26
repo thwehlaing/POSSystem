d0ee0ed14430d8b4f9127313f3c1388ed42a538d
thwehlaing <thwehlaing.1993@gmail.com>
2023-02-26 17:27:52 +0630

    by omk

---
+++ Entity/DataBase/pr_stockpackaging_insert.sql
@@ -0,0 +1,24 @@
+﻿BEGIN TRY 
+ Drop Procedure [dbo].[pr_stockpackaging_insert]
+END try
+BEGIN CATCH END CATCH 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[pr_stockpackaging_insert] 
+	-- Add the parameters for the stored procedure here
+	@ItemCD varchar(12),
+	@PackTypeCode varchar(10),
+	@Qty int,
+	@CreatedUser varchar(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	insert into StockPackaging(ItemCD,PackTypeCode,Qty,CreatedDate,CreatedUser) values(@ItemCD,@PackTypeCode,@Qty,GETDATE(),@CreatedUser)
+END
+GO
